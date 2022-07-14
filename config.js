1496a3903d88b6a502b52b85de2bab2b5ff58eeb
ThongDen <94816883+ThongDen@users.noreply.github.com>
2022-07-14 20:43:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,14 @@
 const CONFIG = {
-    titleWeb: "Dành cho bạn...",
+    titleWeb: "Dành cho Milo ",
     introTitle: 'Bạn iu ơi',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    introDesc: `Trái đất có 7 tỉ người mà sao t chỉ thấy mỗi m dể thưn z`,
+    btnIntro: 'HiHi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnYes: 'Đúng :3',
+    btnNo: 'Không nha',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi cho bạn',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'iu bạn mãi mãi :3',
